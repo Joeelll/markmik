dd16b01298b35b674fc763849938a94bc8dbc077
TomAbel <33309474+TomAbel@users.noreply.github.com>
2017-11-07 21:39:44 +0200

    Tres

--- eikannata/eikannata/Program.cs
+++ eikannata/eikannata/Program.cs
@@ -21,39 +21,58 @@ namespace eikannata
 
             Console.WriteLine("Which file you do want to select?");
             var filename = Console.ReadLine();
-            Console.WriteLine("Whaddya wanna do? (read/write/new file)");
+            Console.WriteLine("Whaddya wanna do? (read/write to/write new file)");
             var choice = Console.ReadLine();
 
             if (choice == "read")
-	        {
-            try
-            {   
-                // Open the text file using a stream reader.
-                string[] lines = System.IO.File.ReadAllLines(@"C:\Markmed\" + filename);
-
-                // Display the file contents by using a foreach loop.
-                System.Console.WriteLine("Opening " + filename);
-                foreach (string line in lines)
+            {
+                try
                 {
-                    // Use a tab to indent each line of the file.
-                    Console.WriteLine("\t" + line);
+                    // Open the text file using a stream reader.
+                    string[] lines = System.IO.File.ReadAllLines(@"C:\Markmed\" + filename);
+
+                    // Display the file contents by using a foreach loop.
+                    System.Console.WriteLine("Opening " + filename);
+                    foreach (string line in lines)
+                    {
+                        // Use a tab to indent each line of the file.
+                        Console.WriteLine("\t" + line);
+                    }
+
+                    // Keep the console window open in debug mode.
+                    Console.WriteLine("Press any key to exit.");
+                    System.Console.ReadKey();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            else if (choice == "write to")
+            {
+                string path = @"c:\Markmed\" + filename;
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine("\n");
+                    sw.WriteLine("--New writing session started--");
+                    sw.WriteLine("\n");
+                    Console.WriteLine("Start typing, press enter when done");
+                    string WritingSession = Console.ReadLine();
+                    sw.WriteLine(WritingSession);
                 }
 
-                // Keep the console window open in debug mode.
-                Console.WriteLine("Press any key to exit.");
-                System.Console.ReadKey();
+                // Open the file to read from.
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-            }
-            
-            else if (choice == "write")
-            {
-            
-}
-}
-}
+        }
+    }
 }
